d7254d040cd2bd410a408f3fcdd34b97232ce1ce
nishmakalathil <159703347+nishmakalathil@users.noreply.github.com>
2025-02-24 22:03:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Use environment variables to set the origin 
-const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173'];
+const allowedOrigins = "https://react-assaignment-4.vercel.app";
 
 app.use(cors({ origin: function (origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
